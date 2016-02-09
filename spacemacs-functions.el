e2cb578e04afc1eb3a751dc400557c83b7ee3a89
Andy Brown <asbrown002@gmail.com>
2016-02-09 13:49:34 -0800

    overwrite projectile test suffix

--- spacemacs-functions.el
+++ spacemacs-functions.el
@@ -14,4 +14,25 @@
     (unless (file-readable-p dir)
       (make-directory dir t))))
 
+(with-eval-after-load 'projectile
+  (defun projectile-test-suffix (project-type)
+    "Find default test files suffix based on PROJECT-TYPE."
+    (cond
+     ((member project-type '(emacs-cask)) "-test")
+     ((member project-type '(rails-rspec ruby-rspec)) "_spec")
+     ((member project-type '(rails-test ruby-test lein-test boot-clj go)) "_test")
+     ((member project-type '(scons)) "test")
+     ((member project-type '(maven symfony)) "Test")
+     ((member project-type '(gulp npm)) "Spec")
+     ((member project-type '(gradle gradlew grails)) "Spec"))))
+
+(defun load-shell-env (command)
+  (interactive)
+  (set
+   (make-local-variable 'process-environment)
+   (split-string
+    (shell-command-to-string
+     command)
+    "\n")))
+
 (provide 'spacemacs-functions)
